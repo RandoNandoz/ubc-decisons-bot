af5bf1f61bea8958ad90112c479e2f79def53a1f
Randy Zhu <mail@randyzhu.com>
2023-03-20 15:55:57 -0700

    Sure it looks uglier, but it fixes the weird button timeout bug

--- src/ApplicantDecision.py
+++ src/ApplicantDecision.py
@@ -3,28 +3,50 @@ Representation of a decision made for an applicant by ubc admissions
 """
 import discord
 
-import Decision
-import Campus
-import UBCProgram
+from Decision import Decision
+from Campus import Campus
+from UBCProgram import UBCProgram
 from dataclasses import dataclass
 from datetime import datetime
+import dateutil
 from typing import Optional
 
 @dataclass
-class ApplicantDecision:
-    discord_username: str
-    discord_id: int
-    campus: Campus
-    program: UBCProgram
-    decision: Decision
-    decision_date: datetime
-    international: bool
-    curriculum: str
-    average: str
-    application_date: datetime
-    comments: Optional[str]
-    intended_major: Optional[str]
-    attachment: discord.Attachment
+class ApplicantDecision():
+    discord_username: str = None
+    discord_id: int = None
+    campus: Campus = None
+    program: UBCProgram = None
+    decision: Decision = None
+    decision_date: datetime = None
+    international: bool = None
+    curriculum: str = None
+    average: str = None
+    application_date: datetime = None
+    comments: Optional[str] = None
+    intended_major: Optional[str] = None
+    attachment: Optional[discord.Attachment] = None
+    msg_id: Optional[int] = None
+
+
+    @staticmethod
+    def from_dict(d: dict):
+        a = ApplicantDecision()
+        a.discord_username = d["discord_username"]
+        a.discord_id = d["discord_id"]
+        a.campus = Campus(d["campus"])
+        a.program = UBCProgram(d["program"])
+        a.decision = Decision(d["decision"])
+        a.decision_date = d["decision_date"]
+        a.international = d["international"]
+        a.curriculum = d["curriculum"]
+        a.average = d["average"]
+        a.application_date = d["application_date"]
+        a.comments = d["comments"]
+        a.intended_major = d["intended_major"]
+        a.attachment = d["attachment"]
+        a.msg_id = d["msg_id"]
+        return a
 
     def __dict__(self):
         return {
@@ -40,6 +62,7 @@ class ApplicantDecision:
             "application_date": self.application_date,
             "comments": self.comments,
             "intended_major": self.intended_major,
-            "attachment": self.attachment.url
+            "attachment": self.attachment.url if self.attachment else None,
+            "msg_id": self.msg_id
         }
 
--- src/DecisionsBot.py
+++ src/DecisionsBot.py
@@ -28,71 +28,78 @@ reviewed_decisions = db["reviewed_decisions"]
 unreviewed_decisions = db["unreviewed_decisions"]
 
 
-async def send_applicant_to_review(applicant_decision: ApplicantDecision):
-    review_channel = bot.get_channel(int(environ["REVIEW_CHANNEL_ID"]))
-    accepted_channel = bot.get_channel(int(environ["ACCEPTED_CHANNEL_ID"]))
-    user = await bot.fetch_user(applicant_decision.discord_id)
-
-    review_embed = discord.Embed(title="New Applicant Decision", color=discord.Color.blurple(),
-                                 description=f"**User:** {user}\n"
-                                             f"**Discord ID:** {applicant_decision.discord_id}\n"
-                                             f"**Campus:** {applicant_decision.campus.value}\n"
-                                             f"**Program:** {applicant_decision.program.value}\n"
-                                             f"**Decision:** {applicant_decision.decision.value}\n"
-                                             f"**Decision Date:** {applicant_decision.decision_date.strftime('%Y-%m-%d')}\n"
-                                             f"**International:** {applicant_decision.international}\n"
-                                             f"**Curriculum:** {applicant_decision.curriculum}\n"
-                                             f"**Average:** {applicant_decision.average}\n"
-                                             f"**Application Date:** {applicant_decision.application_date.strftime('%Y-%m-%d')}\n"
-                                             f"**Comments:** {applicant_decision.comments}\n"
-                                             f"**Intended Major:** {applicant_decision.intended_major}\n")
-
-    review_embed.set_image(url=user.display_avatar)
-
-    class ReviewView(discord.ui.View):
-        @discord.ui.button(label="Accept", style=discord.ButtonStyle.green, emoji="✅")
-        async def accept(self, button: discord.ui.Button, interaction: discord.Interaction):
-            reviewed_decisions.insert_one(applicant_decision.__dict__())
-            unreviewed_decisions.delete_one({"discord_id": applicant_decision.discord_id})
-            await interaction.response.send_message("Accepted", ephemeral=True)
-            accepted_embed = discord.Embed(
-                title=f"{user} {applicant_decision.decision.value} for {applicant_decision.program.value} at UBC {applicant_decision.campus.value}!",
-                color=discord.Color.blurple(), description=f"**User:** {user}\n"
-                                                           f"**Campus:** {applicant_decision.campus.value}\n"
-                                                           f"**Program:** {applicant_decision.program.value}\n"
-                                                           f"**Decision:** {applicant_decision.decision.value}\n"
-                                                           f"**Decision Date:** {applicant_decision.decision_date.strftime('%Y-%m-%d')}\n"
-                                                           f"**International:** {applicant_decision.international}\n"
-                                                           f"**Curriculum:** {applicant_decision.curriculum}\n"
-                                                           f"**Average:** {applicant_decision.average}\n"
-                                                           f"**Application Date:** {applicant_decision.application_date.strftime('%Y-%m-%d')}\n"
-                                                           f"**Comments:** {applicant_decision.comments}\n"
-                                                           f"**Intended Major:** {applicant_decision.intended_major}\n"
-                                                           f"**Attachment:** {applicant_decision.attachment}\n"
-
-            )
-
-            accepted_embed.set_image(url=user.display_avatar)
-
-            await accepted_channel.send(embed=accepted_embed)
-
-            # button.disabled = True
-
-            await interaction.message.delete()
-
-        @discord.ui.button(label="Delete", style=discord.ButtonStyle.red, emoji="❌")
-        async def reject(self, button: discord.ui.Button, interaction: discord.Interaction):
-            unreviewed_decisions.delete_one({"discord_id": applicant_decision.discord_id})
-            await interaction.response.send_message(f"Rejected, make sure to PM why <@{applicant_decision.discord_id}>",
-                                                    ephemeral=True)
-            await interaction.message.delete()
-
-    await review_channel.send(embed=review_embed, view=ReviewView())
-
-
 @bot.event
 async def on_ready():
     print("Bot is ready")
+
+
+# default callable is noop https://stackoverflow.com/questions/690622/whats-a-standard-way-to-do-a-no-op-in-python
+async def send_applicant_to_channel(applicant: ApplicantDecision, channel: discord.TextChannel,
+                                    message_cb: callable = lambda *x, **y: None):
+    user = await bot.fetch_user(applicant.discord_id)
+    embed = discord.Embed(
+        title=f"{user} {applicant.decision.value} for {applicant.program.value} at UBC {applicant.campus.value}!",
+        color=discord.Color.blurple(), description=f"**User:** {user}\n"
+                                                   f"**Campus:** {applicant.campus.value}\n"
+                                                   f"**Program:** {applicant.program.value}\n"
+                                                   f"**Decision:** {applicant.decision.value}\n"
+                                                   f"**Decision Date:** {applicant.decision_date.strftime('%Y-%m-%d')}\n"
+                                                   f"**International:** {applicant.international}\n"
+                                                   f"**Curriculum:** {applicant.curriculum}\n"
+                                                   f"**Average:** {applicant.average}\n"
+                                                   f"**Application Date:** {applicant.application_date.strftime('%Y-%m-%d')}\n"
+                                                   f"**Comments:** {applicant.comments}\n"
+                                                   f"**Intended Major:** {applicant.intended_major}\n"
+                                                   f"**Attachment:** {applicant.attachment}\n")
+
+    sent_message = await channel.send(embed=embed)
+    message_cb(applicant, sent_message)
+
+async def get_unreviewed_decisions() -> list[str]:
+    users = [d['discord_username'] for d in unreviewed_decisions.find({})]
+    unique_users = list(set(users))
+    return unique_users
+
+
+@bot.slash_command(name="accept_all", description="Review a decision")
+async def accept_all(ctx: discord.ApplicationContext):
+    for d in unreviewed_decisions.find({}):
+        member = await bot.fetch_user(d["discord_id"])
+        await ctx.respond(f"Accepting {member.mention}")
+        await accept(ctx, member)
+
+# noinspection PyTypeChecker
+@bot.slash_command(name="accept", description="Accept a decision")
+async def accept(ctx: discord.ApplicationContext, applicant: discord.Option(discord.Member, description="The applicant to accept", required=True, autocomplete=get_unreviewed_decisions)):
+    db_applicant = ApplicantDecision.from_dict(unreviewed_decisions.find_one({"discord_id": applicant.id}))
+    if db_applicant is None:
+        await ctx.respond(f"Could not find {applicant.mention} in the database!")
+        return
+    # guaranteed to be a discord.TextChannel object
+    # noinspection PyTypeChecker
+    await send_applicant_to_channel(db_applicant, bot.get_channel(int(environ["ACCEPTED_CHANNEL_ID"])))
+    review_channel: discord.TextChannel = await bot.fetch_channel(int(environ["REVIEW_CHANNEL_ID"]))
+    review_msg: discord.Message = await review_channel.fetch_message(db_applicant.msg_id)
+    await review_msg.delete()
+    unreviewed_decisions.delete_one({"discord_id": applicant.id})
+    reviewed_decisions.insert_one(db_applicant.__dict__())
+    await ctx.respond(f"Accepted {applicant.mention}!")
+
+
+# noinspection PyTypeChecker
+@bot.slash_command(name="reject", description="Reject a decision")
+async def reject(ctx: discord.ApplicationContext, applicant: discord.Option(discord.Member, description="The applicant to reject", required=True, autocomplete=get_unreviewed_decisions)):
+    try:
+        applicant_decision = ApplicantDecision.from_dict(unreviewed_decisions.find_one({"discord_id": applicant.id}))
+        review_channel: discord.TextChannel = await bot.fetch_channel(int(environ["REVIEW_CHANNEL_ID"]))
+        db_applicant = unreviewed_decisions.find_one({"discord_id": applicant.id})
+        review_msg: discord.Message = await review_channel.fetch_message(db_applicant["msg_id"])
+        await review_msg.delete()
+        await ctx.respond(f"Rejected {applicant.mention}!")
+        unreviewed_decisions.delete_one(db_applicant)
+    except TypeError:
+        await ctx.respond(f"Could not find {applicant.mention} in the database!")
+
 
 
 @bot.slash_command(name="decision", description="Submit your UBC admissions decision for review")
@@ -101,17 +108,17 @@ async def decision(ctx: discord.ApplicationContext, campus: discord.Option(Campu
                                                                            required=True),
                    program: discord.Option(UBCProgram, description="The UBC program you applied to", required=True),
                    applicant_decision: discord.Option(Decision, description="The decision you received from UBC",
-                                                      required=True),
-                   decision_date: discord.Option(str, description="The date you received your decision, preferably in YYYY-MM-DD",
-                                                 required=True),
+                                                      required=True), decision_date: discord.Option(str,
+                                                                                                    description="The date you received your decision, preferably in YYYY-MM-DD",
+                                                                                                    required=True),
                    international: discord.Option(bool, description="Whether you are an international student or not",
                                                  required=True), curriculum: discord.Option(str,
                                                                                             description="The curriculum you're using, for example, BC, AB, IB, American, etc.",
                                                                                             required=True),
                    average: discord.Option(str, description="Your average, for example, 90.5", required=True, min=0,
-                                           max=100),
-                   application_date: discord.Option(str, description="The date you applied to UBC, preferably in YYYY-MM-DD",
-                                                    required=True),
+                                           max=100), application_date: discord.Option(str,
+                                                                                      description="The date you applied to UBC, preferably in YYYY-MM-DD",
+                                                                                      required=True),
                    comments: discord.Option(str, description="Any comments you have about your decision",
                                             required=False), intended_major: discord.Option(str,
                                                                                             description="The program you intended to apply to, if you applied to a different program",
@@ -119,8 +126,6 @@ async def decision(ctx: discord.ApplicationContext, campus: discord.Option(Campu
                    attachment: discord.Option(discord.Attachment, description="An attachment to your decision",
                                               required=False)):
     try:
-        # decision_date = datetime.strptime(decision_date, "%Y-%m-%d")
-        # application_date = datetime.strptime(application_date, "%Y-%m-%d")
         decision_date = dateutil.parser.parse(decision_date)
         application_date = dateutil.parser.parse(application_date)
         applicant_decision = ApplicantDecision(discord_username=ctx.author.name, discord_id=ctx.author.id,
@@ -130,16 +135,19 @@ async def decision(ctx: discord.ApplicationContext, campus: discord.Option(Campu
                                                application_date=application_date, comments=comments,
                                                intended_major=intended_major, attachment=attachment)
 
-        unreviewed_decisions.insert_one(applicant_decision.__dict__())
+        def add_msg_id_to_applicant(applicant: ApplicantDecision, msg: discord.Message):
+            applicant.msg_id = msg.id
+            unreviewed_decisions.insert_one(applicant_decision.__dict__())
 
-        await send_applicant_to_review(applicant_decision)
+        # REVIEW_CHANNEL_ID is guaranteed to be a discord.TextChannel object
+        # noinspection PyTypeChecker
+        await send_applicant_to_channel(applicant_decision,
+                                        await bot.fetch_channel(int(environ["REVIEW_CHANNEL_ID"])), add_msg_id_to_applicant)
 
         await ctx.respond(
             "Success! Your decision has been submitted for review by the mods. Expect a response within 24 hours")
     except ValueError:
-        await ctx.respond("Invalid date format, please use YYYY-MM-DD")
-    except:
-        await ctx.respond("An error occurred, pinging the dev: <@215994239920766977>")
-
+        await ctx.respond(
+            "There was an issue parsing your date format, please use YYYY-MM-DD")
 
 bot.run(environ["DISCORD_TOKEN"])
